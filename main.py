2d0db39a531047eaa5868f0542b1f00a9e371085
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2025-10-10 12:55:36 +0300

    structure becomes more complex

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import xgboost as xgb
 
-df = pd.read_excel('Data.xlsx', sheet_name='Данные')
+df = pd.read_excel('data/data.xlsx', sheet_name='Данные')
 df['Время'] = pd.to_datetime(df['Время'])
 
 period = 12
