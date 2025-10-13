0cce8e33ada7a1a9cbc3b537c8689dd029344264
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2025-10-13 16:04:28 +0300

    welcome unittests

--- scripts/prepare_data.py
+++ scripts/prepare_data.py
@@ -1,7 +1,7 @@
 import sys
 from pathlib import Path
-sys.path.append(str(Path(__file__).parent.parent))
 
+sys.path.append(str(Path(__file__).parent.parent))
 from src.excel_file import excel_file
 from src.csv_file import csv_file
 
