ea302cdfe199da5bf69d99c2e17c54f0993b9c42
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2025-10-15 18:48:10 +0300

    scripts on there places

--- scripts/main.py
+++ scripts/main.py
@@ -1,3 +1,7 @@
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent.parent))
 from src.csv_file import csv_file
 from src.xgb_predictor import xgb_predictor
 
@@ -8,7 +12,7 @@ This is mvp file. I edit it and later it will be removed.
 (
     xgb_predictor(
         df=(
-            csv_file("data/prepared_data.csv")
+            csv_file("../data/prepared_data.csv")
             .load_df()
         ),
         target_column="Deals"
