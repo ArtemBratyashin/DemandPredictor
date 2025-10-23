9d1b4b725b2a0c64344d758490adeea02bc843aa
Artem Bratyashin <bratiashin.aa21@physics.msu.ru>
2025-10-23 15:59:05 +0300

    Updated class for API

--- src/predictor.py
+++ src/predictor.py
@@ -1,6 +1,7 @@
 import joblib
 import os
 import pandas as pd
+from pathlib import Path
 
 """
 Loads a trained model and predicts for the latest row of the provided DataFrame.
@@ -14,8 +15,8 @@ Example:
 
 class Predictor:
 
-    def __init__(self, model_folder: str):
-        self.__model_folder = model_folder
+    def __init__(self, model_folder: str|Path):
+        self.__model_folder = Path(model_folder)
 
     """
     Predicts the target for the next month.
@@ -24,12 +25,12 @@ class Predictor:
         model = self.__load_model(self.__model_folder)
         features = self.__last_month_features(self.__model_folder)
         prediction = model.predict(features)
-        return prediction[0]
+        return float(prediction[0])
 
     """
     Gives last month features for prediction.
     """
-    def __last_month_features(self, model_folder: str) -> pd.DataFrame:
+    def __last_month_features(self, model_folder: str|Path) -> pd.DataFrame:
         features_path = os.path.join(model_folder, "features.csv")
         features = pd.read_csv(features_path)
         last_features = features.drop(columns=['Month'], errors='ignore').iloc[[-1]]
@@ -38,7 +39,7 @@ class Predictor:
     """
     Loads model from folder.
     """
-    def __load_model(self, model_folder: str) -> joblib.load:
+    def __load_model(self, model_folder: str|Path) -> joblib.load:
         model_name = os.path.basename(os.path.normpath(model_folder))
         model_path = os.path.join(model_folder, model_name + ".joblib")
         model = joblib.load(model_path)
